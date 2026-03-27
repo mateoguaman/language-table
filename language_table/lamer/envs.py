74fedb99806c037dae95adfe3f4728c4eb67eb31
memmelma <marius.memmel@gmx.net>
2026-03-27 08:40:35 -0700

    fix group seeding

--- language_table/lamer/envs.py
+++ language_table/lamer/envs.py
@@ -326,11 +326,11 @@ class LanguageTableMultiProcessEnv:
     def reset(self):
         """Reset all envs with unique seeds."""
         if self.is_train:
-            seeds = np.random.randint(0, 2**16 - 1, size=self.num_processes)
+            seeds = np.random.randint(0, 2**16 - 1, size=self.env_num)
         else:
-            seeds = np.random.randint(2**16, 2**32 - 1, size=self.num_processes)
+            seeds = np.random.randint(2**16, 2**32 - 1, size=self.env_num)
 
-        seeds = seeds.tolist()
+        seeds = np.repeat(seeds, self.group_n).tolist()
 
         futures = [w.reset.remote(seed=s) for w, s in zip(self.workers, seeds)]
         results = ray.get(futures, timeout=self.timeout)
